9535435aaa3141ea5ff24a41dfc9b59f1f860e20
AgEButton <89423088+AgEButton@users.noreply.github.com>
2022-07-14 10:41:44 -0400

    Update class-genesis.php
    Added aria-label to search icon for accessibility

--- src/class-genesis.php
+++ src/class-genesis.php
@@ -147,12 +147,16 @@ class Genesis {
 		if ( is_active_sidebar( 'af4-header-right' ) ) {
 
 			$search  = '<div class="title-bars cell medium-shrink title-bar-right">';
-			$search .= '<div class="title-bar title-bar-search"><button class="search-icon" type="button" data-toggle="header-search"></button><div class="title-bar-title">Search</div>';
+			$search .= '<div class="title-bar title-bar-search"><button class="search-icon" type="button" aria-label="Search" data-toggle="header-search"></button><div class="title-bar-title">Search</div>';
 			$search  = $af_required->add_nav_search_widget_area( $search );
 			$search  = str_replace( 'id="header-search', 'data-toggler=".hide-for-medium" id="header-search', $search );
 			$search .= '</div></div>';
 
 			$output .= $search;
+
+		}
+
+		return $output;
 
 		}
 
